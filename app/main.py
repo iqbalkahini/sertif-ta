fb21039e2724e9e5eba827dfd56235505152dd7c
h4v1d <cukurromli@gmail.com>
2026-01-14 10:57:45 +0700

    change title swagger docs

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ from brotli_asgi import BrotliMiddleware
 from app.api.v1.router import api_router
 
 app = FastAPI(
-    title="Sistem Surat Menyurat API",
-    description="API for generating official dynamic letters (Surat Dinas, Surat Tugas, etc.)",
+    title="surat - sertif mcsrvce",
+    description="surat - tugas(done), pengantaran - monitoring1 - monitoring 2 - penjemputan"
+                "lembar-persetujuan untuk siswa konfirmasi dan ttd ke industri",
     version="1.0.0"
 )
 
